29fdc9b2137077497d56efac24bb72ef33e28638
KamranAlili <kamran-alili@mail.ru>
2020-09-12 15:34:33 +0400

    resume

---
+++ ResumeDbApp/src/main/java/com/company/dao/inter/AbstractDAO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.dao.inter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author KamranAlili
+ */
+public abstract class AbstractDAO {
+    
+     public Connection connect() throws Exception {
+        String url = "jdbc:mysql://localhost:3306/resume";
+        String username = "root";
+        String password = "Qafqaz2018!!";
+        Connection c = DriverManager.getConnection(url, username, password);
+        return c;
+    }
+
+}
---
+++ ResumeDbApp/src/main/java/com/company/main/Context.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.main;
+
+import com.company.dao.impl.UserDaoImpl2;
+import com.company.dao.inter.UserDaoInter;
+
+/**
+ *
+ * @author KamranAlili
+ */
+public class Context {
+    
+    public static UserDaoInter instanceUserDao() {
+    return new UserDaoImpl2();
+    }
+    
+    
+}
